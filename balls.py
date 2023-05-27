b9cae2cb2258f63721833305f2e4816e21f1ca55
RobiAKiraly <129603776+RobiAKiraly@users.noreply.github.com>
2023-05-27 17:09:48 +0300

    balls.py

--- balls.py
+++ balls.py
@@ -2,12 +2,12 @@ from pygame import*
 
 class balls(sprite.Sprite): #in the parentheses it is indicated that the class inherits from the Sprite class
 
-    image_path = ""
+    image_path = "f1.jpg"
     def __init__(self, x, y):
        sprite.Sprite.__init__(self) #mandatory calling of the constructor of the parent class
        # each sprite should store the image property—its image
        self.image = image.load(self.image_path)
-       self.image = transform.scale(self.image, (64,64)) # resize the image
+       self.image = transform.scale(self.image, (15,15)) # resize the image
  
        # each sprite should store the rect property—the rectangle into which it is inscribed
        self.rect = self.image.get_rect() #we get a rectangle from the image
@@ -18,5 +18,5 @@ class balls(sprite.Sprite): #in the parentheses it is indicated that the class i
         return self.rect.y
        #method defining the sprite’s movement
     def update(self):
-        self.rect.y += 1
+        self.rect.y -= 1
         
