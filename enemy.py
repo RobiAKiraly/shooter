97efa3cf93d5eb1c7a029873a863431f7eab1090
RobiAKiraly <129603776+RobiAKiraly@users.noreply.github.com>
2023-05-27 17:10:19 +0300

    enemy.py

--- enemy.py
+++ enemy.py
@@ -1,1 +1,14 @@
 from pygame import *
+
+class Enemy(sprite.Sprite):
+    def __init__(self, image_path,x,y):
+        sprite.Sprite.__init__(self)
+        self.image=image.load("zolika.jpg")
+        self.image = transform.scale(self.image, (30,30))
+
+        self.rect = self.image.get_rect() 
+        
+        self.start_x = x
+        self.start_y = y
+        self.rect.x = x
+        self.rect.y = y 
